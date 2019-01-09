9dbe0ae9ea0508bd36e7cb3d07d93a0f7092596b
Joana Souza <jpsouza@MacBook-Pro-292.local>
2019-01-09 16:25:06 -0300

    Add temperature table

--- src/Components/Temperature/index.js
+++ src/Components/Temperature/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import './temperature.css';
 
-const Temperature = () => <h2 class="temperature-header">Capitais</h2>;
+const Temperature = () => (
+    <>
+    <h2 class="temperature-header">Capitais</h2>
+    <div class="weather-chart">
+        <table>
+            <tr>
+                <th>Min</th>
+                <th>Max</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>18</td>
+                <td>27</td>
+                <td>Rio de Janeiro</td>
+                </tr>
+        </table>
+        </div>
+    </>
+)
+
 
 export default Temperature;
